6de834fe130a9b1cf107edd45a746a604dd993c1
thomastsai <thomas@life100.cc>
2014-09-22 17:23:00 +0800

    adding C apis header

--- capture-v4l2.c
+++ capture-v4l2.c
@@ -48,6 +48,57 @@ int SetAutoWhiteBalance(int fd, int enable)
 	return GetAutoWhiteBalance(fd) == enable;
 }
 
+int getVideoFMT(int fd)
+{
+	int support_grbg10 = 0;
+	struct v4l2_fmtdesc fmtdesc = {0};
+    fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    char fourcc[5] = {0};
+    char c, e;
+    printf("\n  FMT : CE Desc\n--------------------\n");
+    while (0 == xioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc))
+    {
+        strncpy(fourcc, (char *)&fmtdesc.pixelformat, 4);
+        if (fmtdesc.pixelformat == V4L2_PIX_FMT_SGRBG10)
+            support_grbg10 = 1;
+        c = fmtdesc.flags & 1? 'C' : ' ';
+        e = fmtdesc.flags & 2? 'E' : ' ';
+        printf("  %s: %c%c, %s\n", fourcc, c, e, fmtdesc.description);
+        fmtdesc.index++;
+    }
+}
+
+int setVideoFMT(int fd)
+{
+    struct v4l2_format fmt = {0};
+    char fourcc[5] = {0};
+
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    fmt.fmt.pix.width = 640;
+    fmt.fmt.pix.height = 480;
+    //fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_BGR24;
+    //fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_GREY;
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+    fmt.fmt.pix.field = V4L2_FIELD_NONE;
+    
+    if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
+    {
+        perror("Setting Pixel Format");
+        //return 1;
+    }else{
+    strncpy(fourcc, (char *)&fmt.fmt.pix.pixelformat, 4);
+    printf( "Selected Camera Mode:\n"
+        "  Width: %d\n"
+        "  Height: %d\n"
+        "  PixFmt: %s\n"
+        "  Field: %d\n",
+        fmt.fmt.pix.width,
+        fmt.fmt.pix.height,
+        fourcc,
+        fmt.fmt.pix.field);
+	}
+}
+
 int print_caps(int fd)
 {
         struct v4l2_capability caps = {};
---
+++ capture-v4l2.h
@@ -0,0 +1,13 @@
+#ifndef _H_CAPTURE-V4L2_H
+#define _H_CAPTURE-V4L2_H
+/*
+	<thomas tsai>thomas@biotrump.com
+*/
+
+int GetAutoWhiteBalance(int fd);
+int SetAutoWhiteBalance(int fd, int enable);
+int getVideoFMT(int fd);
+int setVideoFMT(int fd);
+
+#endif
+
